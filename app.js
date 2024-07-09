36ca1a8f1f7ee7786b31f4988b01ef6e793fe27a
NehaSharma4122 <rmneha1234@gmail.com>
2024-07-09 11:35:14 +0530

    FINALCOMMIT

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-require ("dotenv").config();
+require("dotenv").config();
 require("./conn/conn");
+const task = require("./models/task");
+const user = require("./models/user");
 
-const cors = require ("cors");
+const cors = require("cors");
 const UserAPI = require("./routes/user");
 const TaskAPI = require("./routes/task");
 
@@ -12,19 +14,30 @@ const TaskAPI = require("./routes/task");
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/v1", UserAPI);
 app.use(express.json());
 app.use("/api/v2", TaskAPI);
 
-
-
-app.use("/",(req,res) => {
-    res.send("Hello from backend side");
-});
-
 const PORT = 1000;
 
-app.listen(PORT,()=>{
-    console.log("Server started");
-})
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await user.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
